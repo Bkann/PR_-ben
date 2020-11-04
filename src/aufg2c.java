e56a6f004cdf036820d536d004434a0ebde2e30d
Bkann <73962044+Bkann@users.noreply.github.com>
2020-11-04 20:07:05 +0100

    Update aufg2c.java

--- src/aufg2c.java
+++ src/aufg2c.java
@@ -6,24 +6,39 @@ import java.util.Scanner;
 				
 				
 		        Scanner input = new Scanner(System.in);
+				
+				
+				System.out.print("was wollen sie berechnen:");
+				 String berechnung = input.nextLine();
 		        
-		          System.out.print("Name des Patienten: ");
-		           String name = input.nextLine();
 		          
-		          System.out.print("Kreatinwert des Patienten: ");
-		           double Kreatinwert = input.nextDouble();
+				System.out.print("Name des Patienten: ");
 		          
-		          System.out.print("Bilirubinwert des Patienten: ");
-		           double Bilirubinwert = input.nextDouble();
+				String name = input.nextLine();
+		          
+		         
+				System.out.print("Kreatinwert des Patienten: ");
+		          
+				double Kreatinwert = input.nextDouble();
+		          
+		       
+				System.out.print("Bilirubinwert des Patienten: ");
+		          
+				double Bilirubinwert = input.nextDouble();
 		           
-		          System.out.print("INR-Wert des Patienten: ");
-		           double INRwert = input.nextDouble();
+		       
+				System.out.print("INR-Wert des Patienten: ");
+		        
+				double INRwert = input.nextDouble();
 				
 				
-		            double meldScore  = (10 * (0.957 * Math.log(Kreatinwert) + 0.378 * Math.log(Bilirubinwert) + 1.12 * Math.log(INRwert) + 0.643) ); 
+		           
+				double meldScore  = (10 * (0.957 * Math.log(Kreatinwert) + 0.378 * Math.log(Bilirubinwert) + 1.12 * Math.log(INRwert) + 0.643) ); 
 				
 				
-			     System.out.printf ("Patient %s hat einen Meld-Score von %.1f." , name, meldScore);
+			     
+				System.out.printf ("Patient %s hat einen Meld-Score von %.1f." , name, meldScore);
+				
 				
 				
 				
